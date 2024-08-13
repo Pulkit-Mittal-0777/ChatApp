f6f07ed1f5a5c29fdee1dd581e830f5766dbe532
Pulkit-Mittal-0777 <148517429+Pulkit-Mittal-0777@users.noreply.github.com>
2024-08-13 13:01:28 +0530

    second commit

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express=require('express');
 
 const { app, server } =require("./SocketIO/server.js");
 const mongoose=require('mongoose');
+const path=require('path');
 const dotenv=require('dotenv');
 const cors=require('cors');
 const cookie_parser=require('cookie-parser')
@@ -28,6 +29,18 @@ app.use(cookie_parser());
 app.use(userRoute);
 
 app.use(messageRoute);
+
+// ----------code for deployment ----------------
+
+if(process.env.NODE_ENV === "production"){
+
+    const dirPath=path.resolve();
+
+    app.use(express.static("./frontend/dist"));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(dirPath, "./frontend/dist" , "index.html"));
+    })
+}
 
 const PORT=process.env.PORT || 5000;
 server.listen(PORT,()=>{
